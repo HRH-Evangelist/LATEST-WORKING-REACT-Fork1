fbcd8e6b84ed58a6e6b9f7677b9758deb0fab6a6
HRH-Evangelist <vatsahary@gmail.com>
2026-02-05 18:10:03 +0530

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App.tsx';
 import Login from './Login.tsx';
 import UserNFC from './UserNFC.tsx';
@@ -8,20 +8,23 @@ import UserRedirect from './UserRedirect.tsx';
 import EditProfile from './EditProfile.tsx';
 import HHL from './HHL.tsx';
 import ResetPassword from './ResetPassword.tsx';
+import { TermsOfService } from './components/Terms.tsx';
+import { PrivacyPolicy } from './components/Privacy.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<HHL />} />
         <Route path="/register" element={<App />} />
         <Route path="/login" element={<Login />} />
         <Route path="/reset-password" element={<ResetPassword />} />
+        <Route path="/terms" element={<TermsOfService />} />
+        <Route path="/privacy" element={<PrivacyPolicy />} />
         <Route path="/user-nfc" element={<UserNFC />} />
         <Route path="/user" element={<UserRedirect />} />
         <Route path="/edit_profile" element={<EditProfile />} />
-        <Route path="/hhl" element={<HHL />} />
-        <Route path="/" element={<Navigate to="/hhl" replace />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
